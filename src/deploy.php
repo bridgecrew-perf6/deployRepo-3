3a0ade6edb4b74e8b30c7efc8143b8de4a178905
Alexey <uadeveloper@gmail.com>
2022-04-12 20:50:25 +0300

    Update deploy.php

--- src/deploy.php
+++ src/deploy.php
@@ -4,7 +4,16 @@ require_once 'vendor/autoload.php';
 #$handler = new \Deploy\Handler();
 #$handler->run();
 
-echo 'AWS_ACCESS_KEY_ID:' . getenv('AWS_ACCESS_KEY_ID');
+echo 'AWS_ACCESS_KEY_ID:' . getenv('AWS_ACCESS_KEY_ID') . "\n";
+echo 'AWS_SECRET_ACCESS_KEY:' . getenv('AWS_SECRET_ACCESS_KEY') . "\n";
+echo 'AWS_REGION:' . getenv('AWS_REGION') . "\n";
+echo 'AWS_CONFIG_SET:' . getenv('AWS_CONFIG_SET') . "\n";
+echo 'AWS_VPC_NAME:' . getenv('AWS_VPC_NAME') . "\n";
+echo 'AWS_SCRIPTS_GROUP_NAME:' . getenv('AWS_SCRIPTS_GROUP_NAME') . "\n";
+echo 'AWS_ADMIN_GROUP_NAME:' . getenv('AWS_ADMIN_GROUP_NAME') . "\n";
+echo 'AWS_FRONTEND_GROUP_NAME:' . getenv('AWS_FRONTEND_GROUP_NAME') . "\n\n";
+
+print_r($argv);
 
 print_r($_ENV);
 print_r($_SERVER);
