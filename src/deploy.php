76c9f9ee57bb9291186f93272149bc3389efb434
Alexey <uadeveloper@gmail.com>
2022-04-12 20:41:07 +0300

    Update deploy.php

--- src/deploy.php
+++ src/deploy.php
@@ -4,5 +4,7 @@ require_once 'vendor/autoload.php';
 #$handler = new \Deploy\Handler();
 #$handler->run();
 
+echo 'AWS_ACCESS_KEY_ID:' . getenv('AWS_ACCESS_KEY_ID');
+
 print_r($_ENV);
 print_r($_SERVER);
